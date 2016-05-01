dd722e432cc297d26e6117934b4b138eca4851e1
s0lst1c3 <solstice@member.fsf.org>
2016-05-01 13:57:45 -0400

    fixed index

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 from flask.ext.socketio import SocketIO, emit
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+app.config['DEBUG'] = True
+socketio = SocketIO(app)
 
-def
+@app.route('/')
+def index():
 
+    return render_template('index.html')
 
 if __name__ == '__main__':
 
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'secret'
-    socketio = SocketIO(app)
-
+    socketio.run(app)
